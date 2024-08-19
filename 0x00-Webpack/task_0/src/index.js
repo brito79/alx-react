ec3c850538007909ca18d9b74f4cb72300a62960
root <root@DESKTOP-7FQKUBN>
2024-08-20 01:40:33 +0200

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+const paragraph1 = $('<p>').text('Holberton Dashboard');
+const paragraph2 = $('<p>').text('Dashboard data for the students');
+const paragraph3 = $('<p>').text('Copyright - Holberton School');
+
+$('body').append(paragraph1,paragraph2,paragraph3);
