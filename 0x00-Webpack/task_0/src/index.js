e3902cc663e1cc29f7e917d7aa5f8a71deb1da03
root <bshayamano2002@gmail.com>
2024-08-20 01:59:03 +0200

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
-import $ from 'jquery';
+const $ = require('jquery');
 
-const paragraph1 = $('<p>').text('Holberton Dashboard');
-const paragraph2 = $('<p>').text('Dashboard data for the students');
-const paragraph3 = $('<p>').text('Copyright - Holberton School');
 
-$('body').append(paragraph1,paragraph2,paragraph3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
